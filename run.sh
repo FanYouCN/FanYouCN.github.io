bc8c84ac71cff0f88dffa1dbd8b841094425a8c0
Fan You <fanyou.cn@gmail.com>
2020-10-08 19:11:34 -0700

    2020 Oct Update
    routine update

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-cd ~/Documents/MyWebsite
-jemdoc index
-jemdoc research
-jemdoc teaching
+cd ~/Documents/Github/FanYouCN.github.io
+python2 jemdoc.py index.jemdoc
+python2 jemdoc.py research.jemdoc
+python2 jemdoc.py teaching.jemdoc
+python2 jemdoc.py industry.jemdoc
