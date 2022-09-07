f023aa9b0cde29c8a6d8964447899141083c8c2d
Fan You <fanyou.cn@gmail.com>
2022-09-06 22:23:51 -0700

    add erdos number

--- run.sh
+++ run.sh
@@ -4,3 +4,7 @@ python jemdoc.py research.jemdoc
 python jemdoc.py teaching.jemdoc
 python jemdoc.py industry.jemdoc
 python jemdoc.py fun.jemdoc
+
+
+# jemdoc relies on python2
+# run conda activate py27, then bash run.sh
